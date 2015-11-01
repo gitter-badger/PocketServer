4572e6eb1599e09e935ce585f75505354a9be674
nickrobson <robson.nicholas1@gmail.com>
2015-11-02 00:30:45 +1100

    Add a working example plugin!

---
+++ ExamplePlugin/src/me/nickrobson/pocketserver/example/ExamplePlugin.java
@@ -0,0 +1,11 @@
+package me.nickrobson.pocketserver.example;
+
+import com.pocketserver.plugin.Plugin;
+
+public class ExamplePlugin extends Plugin {
+	
+	public void onEnable() {
+		System.out.println("Hello world!");
+	}
+
+}
