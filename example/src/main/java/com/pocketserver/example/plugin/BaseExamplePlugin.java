2a1a1fd233e7a3fe26e30d1b0b2e1cf9fcf849fc
nickrobson <robson.nicholas1@gmail.com>
2015-11-03 11:20:27 +1100

    Stuffs

--- example/src/main/java/com/pocketserver/example/plugin/BaseExamplePlugin.java
+++ example/src/main/java/com/pocketserver/example/plugin/BaseExamplePlugin.java
@@ -3,7 +3,7 @@ package com.pocketserver.example.plugin;
 import com.pocketserver.plugin.Plugin;
 import com.pocketserver.plugin.PluginData;
 
-@PluginData(name = "BaseExample", version = "1.0.0", description = "A low example dependency", dependency = "adhaddada")
+@PluginData(name = "BaseExample", version = "1.0.0", description = "A low example dependency")
 public class BaseExamplePlugin extends Plugin {
 
     @Override
--- example/src/main/java/com/pocketserver/example/plugin/CircularDependencyTestPlugin.java
+++ example/src/main/java/com/pocketserver/example/plugin/CircularDependencyTestPlugin.java
@@ -3,7 +3,7 @@ package com.pocketserver.example.plugin;
 import com.pocketserver.plugin.Plugin;
 import com.pocketserver.plugin.PluginData;
 
-@PluginData(name = "Circular", version = "1.0.0", description = "A low example dependency", dependency = "Example")
+@PluginData(name = "Circular", version = "1.0.0", description = "A low example dependency", dependency = "BaseExample")
 public class CircularDependencyTestPlugin extends Plugin {
 
     @Override
