fc1c79c37e32b8154748fb1c1ff5a9bd9185ffa3
nickrobson <robson.nicholas1@gmail.com>
2015-11-04 12:29:37 +1100

    Command interface methods

--- api/src/main/java/com/pocketserver/command/Command.java
+++ api/src/main/java/com/pocketserver/command/Command.java
@@ -1,4 +1,13 @@
 package com.pocketserver.command;
 
 public interface Command {
+
+    String getName();
+
+    String[] getAliases();
+
+    String getHelpText(CommandExecutor executor);
+
+    void exec(CommandExecutor executor, String used, String[] args);
+
 }
